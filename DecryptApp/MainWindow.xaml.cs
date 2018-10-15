2cd757fa6ae1be9ba05f3bc222582b26e6f7f235
Ethan <ethan.bell@student.curtin.edu.au>
2018-10-15 13:54:10 +0800

    Code added to encrypt a file. Code added to verify if the file before and after encryption/decryption are the same

--- DecryptApp/MainWindow.xaml.cs
+++ DecryptApp/MainWindow.xaml.cs
@@ -22,7 +22,32 @@ namespace Enc_Dec
             InitializeComponent();
         }
 
+        /**
+         * Handles the encryption call to encrypt a file for validation
+         * */
+        private void encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileBrowser = new OpenFileDialog();
+            string file, contents;
+            byte[] encrypted;
 
+            if (fileBrowser.ShowDialog() == true)
+            {
+                file = fileBrowser.FileName;
+                contents = File.ReadAllText(file);
+
+                encrypted = encrypt(contents, aesObj.Key, aesObj.IV);
+
+                try
+                {
+                    file = file.Substring(0, file.Length - 4);
+                    File.WriteAllBytes(file + "Encrypt.txt", encrypted);
+
+                    MessageBox.Show("File successfully encrypted");
+                }
+                catch (IOException) { MessageBox.Show("Problem saving file"); }
+            }
+        }
 
         /**
          * Select the file that we want to decrypt
@@ -110,6 +135,78 @@ namespace Enc_Dec
             catch (Exception e) { MessageBox.Show("Error " + e.Message); }
 
             return plaintext;
+        }
+
+        /**
+         * Method to encrypt a string into a byte array
+         * Code referenced from: https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.aes?view=netframework-4.7.2
+        */
+        private byte[] encrypt(string plainText, byte[] key, byte[] IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            byte[] encrypted = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            try
+            {
+                using (Aes aesAlg = Aes.Create())
+                {
+                    aesAlg.Key = key;
+                    aesAlg.IV = IV;
+
+                    // Create an encryptor to perform the stream transform.
+                    ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                    // Create the streams used for encryption.
+                    using (MemoryStream msEncrypt = new MemoryStream())
+                    {
+                        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                            {
+                                //Write all data to the stream.
+                                swEncrypt.Write(plainText);
+                            }
+                            encrypted = msEncrypt.ToArray();
+                        }
+                    }
+                }
+            }
+            catch (Exception e) { MessageBox.Show("Error " + e.Message); }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        /**
+        * This method compares the file before and after the encrypt/decrypt process and shows the user if it was successsful
+        */
+        private void verifyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string path = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string before = File.ReadAllText(path + "\\EncDecTest.txt");
+                string after = File.ReadAllText(path + "\\EncDecTestDecrypt.txt");
+
+                if (before.Equals(after))
+                {
+                    MessageBox.Show("File successfully encrypted and decrypted");
+                }
+                else
+                {
+                    MessageBox.Show("File could not be encrypted and decrypted");
+                }
+            }
+            catch (IOException) { MessageBox.Show("Problem opening files"); }
         }
     }
 }
